c0f87c8103f4181a72be5f17311907ec35447aae
M.W.R. Prabuddhika <117188107+PRABUDDHIKAMWR@users.noreply.github.com>
2025-08-12 19:29:53 +0530

    Update setup.py

--- youtube-sentiment-project/setup.py
+++ youtube-sentiment-project/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'youtube',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'RP',
+    author_email= 'ruwaniprabuddhika@gmail.com',
     packages= find_packages(),
     install_requires = []
 
